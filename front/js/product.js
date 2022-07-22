8bc0abda6c064b9942216ad2edc975c9080ea2c7
Kevin Paté <eleeckevin@gmail.com>
2022-07-22 19:22:50 +0200

    Ajout du panier dans le localstorage terminé. Commentaires à rajoutés en js pour expliquer le code

--- front/js/product.js
+++ front/js/product.js
@@ -1,8 +1,12 @@
+// Récupération de l'ID de chaque produit grâche à la méthode URLSearchParams
+
 const url_id = window.location.search;
 const params = new URLSearchParams(url_id);
 const id = params.get('id');
 
 console.log(id);
+
+// Récupération des éléments du HTML pour pouvoir les relier aux produits (titre, prix etc...)
 
 let title = document.getElementById('title');
 let price = document.getElementById('price');
@@ -10,6 +14,7 @@ let description = document.getElementById('description');
 let itemsImg = document.querySelector('.item__img');
 let colorSelect = document.getElementById('colors');
 
+// Appel de l'API avec fetch suivi de l'id du produit cliqué
 
 fetch(`http://localhost:3000/api/products/${id}`)
     .then(function (response) {
@@ -24,6 +29,8 @@ fetch(`http://localhost:3000/api/products/${id}`)
             description.innerHTML = value.description,
             itemsImg.innerHTML = `<img src="${value.imageUrl}" alt="${value.altTxt}"></img>`;
 
+        // On parcours avec une boucle la valeur des couleurs de chaque produit
+
         for (let color of value.colors) {
             let colors = document.createElement('option');
             colorSelect.appendChild(colors);
@@ -31,8 +38,37 @@ fetch(`http://localhost:3000/api/products/${id}`)
                 colors.innerHTML = color
         }
 
+        // On crée une variable addCart qui écoutera l'évènement du clique sur le bouton "Ajouter au panier"
 
+        let addCart = document.getElementById('addToCart');
+        let quantity = document.getElementById('quantity');
+
+        addCart.addEventListener('click', function () {
+
+            let addProducts = {                        // On prend les valeurs que l'on veut garder au clique du bouton "Ajouter au panier"
+                nomProduit: value.name,
+                Quantité: quantity.value,
+                Couleur: colorSelect.value,
+                Prix: value.price,
+                imageProduit: value.imageUrl
+            }
+
+            let dataStorage = JSON.parse(localStorage.getItem('Products'))
+            console.log(dataStorage);
+
+            if (dataStorage) {
+                dataStorage.push(addProducts)
+                localStorage.setItem('Products', JSON.stringify(dataStorage))
+
+            } else {
+                dataStorage = []
+                dataStorage.push(addProducts)
+                localStorage.setItem('Products', JSON.stringify(dataStorage))
+                console.log(dataStorage)
+            }
+        })
     })
+
     .catch(function (err) {
         alert('Une erreur est survenue')
         console.log(err);
